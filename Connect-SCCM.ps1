dc4ce99b07c30b3d74d74581b56ad5ecec61ec82
bmillerb <brett.miller@balfourbeatty.com>
2017-07-07 16:44:03 +0100

    re-added server for testing

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -5,7 +5,7 @@ function Connect-SCCM {
             Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
 
             #Create PSDrive with credentials that have rights in SCCM
-            New-PSDrive -Name PR1 -PSProvider CMSite -Root 'sccm.domain.com' -Credential (Get-Credential)
+            New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential (Get-Credential)
 
             #Setting the working directory to PR1
             Set-Location "PR1:" 
