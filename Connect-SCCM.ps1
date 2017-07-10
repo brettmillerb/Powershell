68ac77c56c551f8dbcef905845d87a7d7597856d
bmillerb <brett.miller@balfourbeatty.com>
2017-07-10 14:56:23 +0100

    Changed scope of drive and module import

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -1,7 +1,7 @@
 function Connect-SCCM {
     process {
         #Import the Configuration Manager Module
-        Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1"
+        Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1" -Global
 
         #Create PSDrive with credentials that have rights in SCCM
         New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential (Get-Credential) -Scope global
