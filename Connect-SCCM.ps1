a765b5bc3af139528baab3be45ae6fd547b1c0e3
bmillerb <brett.miller@balfourbeatty.com>
2017-07-10 12:26:50 +0100

    Changed module import behaviour

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -2,10 +2,10 @@ function Connect-SCCM {
     #[CmdletBinding()]
         process {
             #Import the Configuration Manager Module
-            Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+            Import-Module 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1'
 
             #Create PSDrive with credentials that have rights in SCCM
-            New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential (Get-Credential)
+            New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential $creds #(Get-Credential)
 
             #Setting the working directory to PR1
             Set-Location "PR1:" 
