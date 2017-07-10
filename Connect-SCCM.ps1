c55712714ba4e29ea64c8843f7134f9d308fddda
bmillerb <brett.miller@balfourbeatty.com>
2017-07-10 12:30:39 +0100

    Updated module path

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -2,7 +2,7 @@ function Connect-SCCM {
     #[CmdletBinding()]
         process {
             #Import the Configuration Manager Module
-            Import-Module 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1'
+            Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1"
 
             #Create PSDrive with credentials that have rights in SCCM
             New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential $creds #(Get-Credential)
