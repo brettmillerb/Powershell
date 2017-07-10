50fc2e63df91310d66addab0f669373dedf97cc0
bmillerb <brett.miller@balfourbeatty.com>
2017-07-10 14:42:42 +0100

    Changed drive mapping to Global to prevent falling out of scope

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -4,9 +4,9 @@ function Connect-SCCM {
         Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1"
 
         #Create PSDrive with credentials that have rights in SCCM
-        New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential (Get-Credential)
+        New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential (Get-Credential) -Scope global
 
         #Setting the working directory to PR1
-        Set-Location "PR1:" 
+        Set-Location "PR1:"
     }
 }
