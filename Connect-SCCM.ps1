aa52c4c9d071bb83e579f59626f6b99083b6b221
bmillerb <brett.miller@balfourbeatty.com>
2017-07-10 12:35:49 +0100

    removed cmdletbinding

--- Connect-SCCM.ps1
+++ Connect-SCCM.ps1
@@ -1,13 +1,12 @@
 function Connect-SCCM {
-    #[CmdletBinding()]
-        process {
-            #Import the Configuration Manager Module
-            Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1"
+    process {
+        #Import the Configuration Manager Module
+        Import-Module "C:\Program Files (x86)\Configuration Manager\Console\bin\ConfigurationManager.psd1"
 
-            #Create PSDrive with credentials that have rights in SCCM
-            New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential $creds #(Get-Credential)
+        #Create PSDrive with credentials that have rights in SCCM
+        New-PSDrive -Name PR1 -PSProvider CMSite -Root 'gbstvsccmpw001.bbds.balfourbeatty.com' -Credential $creds #(Get-Credential)
 
-            #Setting the working directory to PR1
-            Set-Location "PR1:" 
-        }
+        #Setting the working directory to PR1
+        Set-Location "PR1:" 
+    }
 }
