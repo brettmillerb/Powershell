a715539e62f453e96c4e7e9f11bfa24d789765d1
bmillerb <brett.miller@balfourbeatty.com>
2017-07-16 23:00:41 +0100

    New File Convert-ImmutableID

---
+++ Convert-ImmutableID.ps1
@@ -0,0 +1,33 @@
+function Convert-ImmutableID {
+    <#
+    .SYNOPSIS
+    Converts O365 ImmutableID to ActiveDirectory objectGUID
+    
+    .DESCRIPTION
+    Converts O365 ImmutableID check cloud user against on-premises
+    
+    .PARAMETER ImmutableID
+    The Immutable ID from O365/AzureAD which is a base-64 encoded version of the AD objectGUID
+    
+    .EXAMPLE
+    Convert-ImmutableID 't3sJlM0QekeUJ32kOEe1hg=='
+    
+    .NOTES
+    You can get the ImmutableID running:
+    Get-AzureADUser -ObjectId brett.miller@balfourbeatty.com | Select-Object immutableid
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [string]$ImmutableID
+    )
+     process {
+        [PSCustomObject]@{
+            objectGUID = New-Object -TypeName guid (,[System.Convert]::FromBase64String($immutableid))
+        }
+     }
+}
